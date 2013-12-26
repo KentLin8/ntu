3179c56f9c511d5e4879ec8f257612057ec9fb3c
kent <kent@codream.tw>
2013-12-26 16:55:16 +0800

    first

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Ntu::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'streams#index'
+  resources :streams
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
