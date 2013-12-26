7918ec56d7f29f74aac2e475212ecb9046214fe8
kent <kentlin@ppolis.com>
2013-12-26 15:34:13 +0800

    initial commit

---
+++ app/controllers/streams_controller.rb
@@ -0,0 +1,10 @@
+class StreamsController < ApplicationController
+  def photos
+
+  end
+
+  def index
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ntu::Application.config.secret_key_base = 'a7ab1a5d6ec5db5c1d22297dec31be34532cc98e5a2a8b03d88c95b4ee926b55ad99efa59a1c6e8f22f4d2d5a0b814336e1d7cbe1a2814471b35e9a0ed368ed9'
