1e2a627d1903ff353791fad80cea7f6c55a5542e
kent <kent@codream.tw>
2013-12-27 15:38:37 +0800

    index

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,10 +1,17 @@
 class StreamsController < ApplicationController
+  require 'rss'
+  
   def photos
 
   end
 
   def index
-
+     begin
+       @latest_blog_posts = RSS::Parser.parse(open('http://blog.streams.tw/rss').read, false).items[0…5]
+     rescue
+       # Do nothing, just continue.  The view will skip the blog section if the feed is nil.
+       @latest_blog_posts = nil
+     end
   end
 
 end
