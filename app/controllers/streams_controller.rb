828b06d327f4a1ad6989d0d498270d1c5e5cfd94
kent <kentlin@ppolis.com>
2014-01-09 02:23:15 +0800

    index

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -14,23 +14,7 @@ class StreamsController < ApplicationController
     end
     @page_title = '荒漠甘泉餐廳-Photos'
 
-    @page_header =  '
-      <!-- Add mousewheel plugin (this is optional) -->
-      <script type="text/javascript" src="/lib/jquery.mousewheel-3.0.6.pack.js"></script>
-
-      <!-- Add fancyBox main JS and CSS files -->
-      <script type="text/javascript" src="/source/jquery.fancybox.js?v=2.1.5"></script>
-      <link rel="stylesheet" type="text/css" href="/source/jquery.fancybox.css?v=2.1.5" media="screen" />
-
-      <!-- Add Button helper (this is optional) -->
-      <link rel="stylesheet" type="text/css" href="/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" />
-      <script type="text/javascript" src="/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
-
-      <!-- Add Thumbnail helper (this is optional) -->
-      <link rel="stylesheet" type="text/css" href="/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" />
-      <script type="text/javascript" src="/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>'
-
-    @page_header +=  ''
+    @page_header =  ''
 
   end
 
